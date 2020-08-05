9a64f904bd43a3cfb2b9320195c7417ccf9bf5a7
darcese <spartans991@yahoo.com>
2020-08-04 21:57:11 -0700

    added redux and implemented user reducer

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -48,7 +48,7 @@ class SignIn extends React.Component {
                         <FormInput name="password" type="password" value={this.state.password} label='password' handleChange={this.handleChange} required />                     
                         <div className='buttons'>
                             <CustomButton type="submit">Sign In</CustomButton>                    
-                            <CustomButton isGoogleSignIn onClick={signInWithGoogle}>Sign In With Google</CustomButton>            
+                            <CustomButton type="button" isGoogleSignIn onClick={signInWithGoogle}>Sign In With Google</CustomButton>            
                         </div>
                          </form>
                 </div>
