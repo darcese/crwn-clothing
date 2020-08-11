b0bd5fe1b8caf03e2e36035d8abfd2cea1685634
darcese <spartans991@yahoo.com>
2020-08-10 22:53:47 -0700

    connected CartItem components with dropdown

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -4,11 +4,11 @@ import './custom-button.styles.scss';
 
 
 //<button class="google-sign-in custom-button">Sign In With Google</button>
-const CustomButton = ({children, isGoogleSignIn, ...otherProps}) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in': ''} custom-button`} {...otherProps}>
+const CustomButton = ({children, isGoogleSignIn,inverted, ...otherProps}) => (
+    <button className={`${inverted ? 'inverted': ''} ${isGoogleSignIn ? 'google-sign-in': ''} custom-button`} {...otherProps}>
         {children}
     </button>
 
-)
+);
 
 export default CustomButton
